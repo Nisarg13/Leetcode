bea008682a107e766e5a9d1c054d01e5fb81c3e8
Arjun <nisargganatra13@gmail.com>
2023-09-18 14:09:16 +0530

    Refactor the code.

--- Easy/Largest_Sum_Whose_Length_Is_k/largest_sum_whose_length_is_k.py
+++ Easy/Largest_Sum_Whose_Length_Is_k/largest_sum_whose_length_is_k.py
@@ -1,13 +1,21 @@
 def find_best_subarray(nums, k):
     curr = 0
+    start_idx = 0  # Initialize the start index of the best subarray
     for i in range(k):
         curr += nums[i]
 
     ans = curr
+    end_idx = k - 1  # Initialize the end index of the best subarray
     for i in range(k, len(nums)):
         curr += nums[i] - nums[i - k]
-        ans = max(ans, curr)
+        if curr > ans:
+            ans = curr
+            start_idx = i - k + 1  # Update the start index of the best subarray
+            end_idx = i  # Update the end index of the best subarray
 
-    return ans
+    best_subarray = nums[start_idx:end_idx + 1]  # Extract the best subarray
+    return ans, best_subarray
 
-print(find_best_subarray([1,2,3,4],2))
+max_sum, subarray = find_best_subarray([1, 2, 3, 4], 2)
+print("Maximum Sum:", max_sum)
+print("Subarray with Maximum Sum:", subarray)
